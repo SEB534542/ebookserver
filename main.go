612c09a6fa294e10c1588a7ee18b0cc2556225a4
SEB534542 <shj.vandermeulen@gmail.com>
2023-01-29 19:27:44 +0100

    x

--- main.go
+++ main.go
@@ -1,10 +1,17 @@
 package main
 
 import (
+	"io"
 	"net/http"
 )
 
 func main() {
 	http.Handle("/", http.FileServer(http.Dir("./assets")))
+	http.HandleFunc("/test", handlerMain)
 	http.ListenAndServe(":8080", nil)
+}
+
+func handlerMain(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/html; charset=utf-8")
+	io.WriteString(w, `hallo<br><a href="./test.pdf">link text</a>`)
 }
